5168e3577a33e7f100345411aada8965e8a332de
Sindhu M <Sindhu.M@godigit.com>
2024-07-23 18:29:35 +0530

    ADD:model created

---
+++ model/user.js
@@ -0,0 +1,10 @@
+export class user{
+    constructor(){
+        this.firstName = "";
+        this.address ="";
+        this.contact ="";
+        this.state ="";
+        this.pincode ="";
+
+    }
+};
